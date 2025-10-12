ca7ceca8bff4205cc9c6025a5173f5043fb0ba81
Michael Ellis <michaelellis003@gmail.com>
2025-10-12 18:16:36 -0500

    try github actions workflow again

--- scripts/05_setup_wif.sh
+++ scripts/05_setup_wif.sh
@@ -9,7 +9,7 @@ for ENV in $PROJECT_ENVS; do
 
     echo "Creating Workload Identity Pool for project $PROJECT_ID..."
     
-    gcloud iam workload-identity-pools create "nextsjs-app-dev" \
+    gcloud iam workload-identity-pools create "nextjs-app-dev" \
     --project="${PROJECT_ID}" \
     --location="global" \
     --display-name="Nextjs app pool"
@@ -17,16 +17,16 @@ for ENV in $PROJECT_ENVS; do
     gcloud iam workload-identity-pools providers create-oidc "nextjs-app-oidc" \
     --project="${PROJECT_ID}" \
     --location="global" \
-    --workload-identity-pool="nextsjs-app-dev" \
+    --workload-identity-pool="nextjs-app-dev" \
     --display-name="Nextjs app pool" \
     --attribute-mapping="google.subject=assertion.sub" \
     --attribute-condition="assertion.repository_owner=='$GITHUB_OWNER'" \
     --issuer-uri="https://token.actions.githubusercontent.com/"
 
-    # gcloud iam service-accounts add-iam-policy-binding $SERVICE_ACCOUNT \
-    # --project="${PROJECT_ID}" \
-    # --role="roles/iam.workloadIdentityUser" \
-    # --member="principalSet://iam.googleapis.com/projects/$PROJECT_NUMBER/locations/global/workloadIdentityPools/nextsjs-app-dev/attribute.repository/$GITHUB_OWNER/$GITHUB_REPO"
+    gcloud iam service-accounts add-iam-policy-binding $SERVICE_ACCOUNT \
+    --project="${PROJECT_ID}" \
+    --role="roles/iam.workloadIdentityUser" \
+    --member="principalSet://iam.googleapis.com/projects/$PROJECT_NUMBER/locations/global/workloadIdentityPools/nextjs-app-dev/attribute.repository/$GITHUB_OWNER/$GITHUB_REPO"
 
 
 done
